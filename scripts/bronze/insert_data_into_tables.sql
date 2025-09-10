6672c02f9a3fb8855b8f02d05051d879c749c7fc
ANKITAKUMARI-azure <ankitakumari211297@outlook.com>
2025-09-10 16:17:36 +0100

    Update insert_data_into_tables.sql

--- scripts/bronze/insert_data_into_tables.sql
+++ scripts/bronze/insert_data_into_tables.sql
@@ -19,25 +19,21 @@ Usage Example:
 
 Create OR Alter Procedure bronze.load_bronze As
 Begin
-	Declare @startTime DateTime, @endTime DateTime
+	Declare @startTime DateTime, @endTime DateTime, @batchStartTime DateTime, @batchEndTime DateTime
 	Begin Try
-
-		PRINT	'===================================================================='
-		PRINT	'Bronze Layer Data Load'
-		PRINT	'===================================================================='
 		
-		PRINT	'===================================================================='
-		PRINT	'Source: CRM'
-		PRINT	'===================================================================='
+		PRINT	'====================================================================';
+		PRINT	'Source: CRM';
+		PRINT	'====================================================================';
 		
-		PRINT	'--------------------------------------------------------------------'
-		PRINT	'Table: bronze.crm_cust_info'
-		PRINT	'--------------------------------------------------------------------'
-
-		set @startTime = GETDATE()
-		PRINT	'>> Truncating bronze.crm_cust_info table'
+		PRINT	'--------------------------------------------------------------------';
+		PRINT	'Table: bronze.crm_cust_info';
+		PRINT	'--------------------------------------------------------------------';
+		set @batchStartTime = GETDATE();
+		set @startTime = GETDATE();
+		PRINT	'>> Truncating bronze.crm_cust_info table';
 		Truncate table bronze.crm_cust_info;
-		PRINT	'>> Bulk Inserting data into bronze.crm_cust_info table'
+		PRINT	'>> Bulk Inserting data into bronze.crm_cust_info table';
 		Bulk Insert bronze.crm_cust_info 
 		From 'C:\Users\Nikhil Kumar\OneDrive\Desktop\MasterClass\Azure\DWH\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		With (
@@ -45,18 +41,18 @@ Begin
 			fieldterminator = ',',
 			tablock
 		);
-		set @endTime = GETDATE()
-		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds'
+		set @endTime = GETDATE();
+		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds';
 		
 		
-		PRINT	'--------------------------------------------------------------------'
-		PRINT	'Table: bronze.crm_prd_info'
-		PRINT	'--------------------------------------------------------------------'
+		PRINT	'--------------------------------------------------------------------';
+		PRINT	'Table: bronze.crm_prd_info';
+		PRINT	'--------------------------------------------------------------------';
 
-		set @startTime = GETDATE()
-		PRINT	'>> Truncating bronze.crm_prd_info table'
+		set @startTime = GETDATE();
+		PRINT	'>> Truncating bronze.crm_prd_info table';
 		Truncate table bronze.crm_prd_info;
-		PRINT	'>> Bulk Inserting data into bronze.crm_prd_info table'
+		PRINT	'>> Bulk Inserting data into bronze.crm_prd_info table';
 		Bulk Insert bronze.crm_prd_info
 		From 'C:\Users\Nikhil Kumar\OneDrive\Desktop\MasterClass\Azure\DWH\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		With (
@@ -64,18 +60,18 @@ Begin
 			fieldterminator = ',',
 			tablock
 		);
-		set @endTime = GETDATE()
-		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds'
+		set @endTime = GETDATE();
+		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds';
 
 		
-		PRINT	'--------------------------------------------------------------------'
-		PRINT	'Table: bronze.crm_sales_details'
-		PRINT	'--------------------------------------------------------------------'
+		PRINT	'--------------------------------------------------------------------';
+		PRINT	'Table: bronze.crm_sales_details';
+		PRINT	'--------------------------------------------------------------------';
 
-		set @startTime = GETDATE()
-		PRINT	'>> Truncating bronze.crm_sales_details table'
+		set @startTime = GETDATE();
+		PRINT	'>> Truncating bronze.crm_sales_details table';
 		Truncate table bronze.crm_sales_details;
-		PRINT	'>> Bulk Inserting data into bronze.crm_sales_details table'
+		PRINT	'>> Bulk Inserting data into bronze.crm_sales_details table';
 		Bulk Insert bronze.crm_sales_details
 		From 'C:\Users\Nikhil Kumar\OneDrive\Desktop\MasterClass\Azure\DWH\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		With (
@@ -83,22 +79,22 @@ Begin
 			fieldterminator = ',',
 			tablock
 		);
-		set @endTime = GETDATE()
-		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds'
+		set @endTime = GETDATE();
+		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds';
 
 
-		PRINT	'===================================================================='
-		PRINT	'Source: ERP'
-		PRINT	'===================================================================='
+		PRINT	'====================================================================';
+		PRINT	'Source: ERP';
+		PRINT	'====================================================================';
 		
-		PRINT	'--------------------------------------------------------------------'
-		PRINT	'Table: bronze.erp_cust_az12'
-		PRINT	'--------------------------------------------------------------------'
+		PRINT	'--------------------------------------------------------------------';
+		PRINT	'Table: bronze.erp_cust_az12';
+		PRINT	'--------------------------------------------------------------------';
 
-		set @startTime = GETDATE()
-		PRINT	'>> Truncating bronze.erp_cust_az12 table'
+		set @startTime = GETDATE();
+		PRINT	'>> Truncating bronze.erp_cust_az12 table';
 		Truncate table bronze.erp_cust_az12;
-		PRINT	'>> Bulk Inserting data into bronze.erp_cust_az12 table'
+		PRINT	'>> Bulk Inserting data into bronze.erp_cust_az12 table';
 		Bulk Insert bronze.erp_cust_az12
 		From 'C:\Users\Nikhil Kumar\OneDrive\Desktop\MasterClass\Azure\DWH\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 		With (
@@ -106,18 +102,18 @@ Begin
 			fieldterminator = ',',
 			tablock
 		);
-		set @endTime = GETDATE()
-		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds'
+		set @endTime = GETDATE();
+		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds';
 
 		
-		PRINT	'--------------------------------------------------------------------'
-		PRINT	'Table: bronze.erp_px_cat_g1v2'
-		PRINT	'--------------------------------------------------------------------'
+		PRINT	'--------------------------------------------------------------------';
+		PRINT	'Table: bronze.erp_px_cat_g1v2';
+		PRINT	'--------------------------------------------------------------------';
 
-		set @startTime = GETDATE()
-		PRINT	'>> Truncating bronze.erp_px_cat_g1v2 table'
+		set @startTime = GETDATE();
+		PRINT	'>> Truncating bronze.erp_px_cat_g1v2 table';
 		Truncate table bronze.erp_px_cat_g1v2;
-		PRINT	'>> Bulk Inserting data into bronze.crm_cust_info table'
+		PRINT	'>> Bulk Inserting data into bronze.crm_cust_info table';
 		Bulk Insert bronze.erp_px_cat_g1v2
 		From 'C:\Users\Nikhil Kumar\OneDrive\Desktop\MasterClass\Azure\DWH\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		With (
@@ -125,18 +121,18 @@ Begin
 			fieldterminator = ',',
 			tablock
 		);
-		set @endTime = GETDATE()
-		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds'
+		set @endTime = GETDATE();
+		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds';
 
 		
-		PRINT	'--------------------------------------------------------------------'
-		PRINT	'Table: bronze.erp_loc_a101'
-		PRINT	'--------------------------------------------------------------------'
+		PRINT	'--------------------------------------------------------------------';
+		PRINT	'Table: bronze.erp_loc_a101';
+		PRINT	'--------------------------------------------------------------------';
 
-		set @startTime = GETDATE()
-		PRINT	'>> Truncating bronze.erp_loc_a101 table'
+		set @startTime = GETDATE();
+		PRINT	'>> Truncating bronze.erp_loc_a101 table';
 		Truncate table bronze.erp_loc_a101;
-		PRINT	'>> Bulk Inserting data into bronze.erp_loc_a101'
+		PRINT	'>> Bulk Inserting data into bronze.erp_loc_a101';
 		Bulk Insert bronze.erp_loc_a101
 		From 'C:\Users\Nikhil Kumar\OneDrive\Desktop\MasterClass\Azure\DWH\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 		With (
@@ -144,18 +140,24 @@ Begin
 			fieldterminator = ',',
 			tablock
 		);
-		set @endTime = GETDATE()
-		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds'
+		set @endTime = GETDATE();
+		PRINT	'>> Load Duration: '+ Cast(datediff(second,@startTime,@endTime) AS NVARCHAR) + ' seconds';
+
+		set @batchEndTime = GETDATE();
+		PRINT '==========================================';
+		PRINT 'Loading Bronze Layer is Completed';
+		PRINT	'>> Complete Batch Load Duration: '+ Cast(datediff(second,@batchStartTime,@batchEndTime) AS NVARCHAR) + ' seconds';
+		PRINT '==========================================';
 
 	End Try
 
 	Begin Catch
-		PRINT '=========================================='
-		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT '==========================================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
 		PRINT 'Error Message' + ERROR_MESSAGE();
 		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
 		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
-		PRINT '=========================================='
+		PRINT '==========================================';
 	End Catch
 
 End;
